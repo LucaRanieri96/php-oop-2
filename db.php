bcf82ea1aff83ea4949916f160f77c13c2779f91
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-17 15:44:12 +0200

    Add db.php with computers array

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$computers = [
+  new Desktop('ASUS PRIME B550-PLUS', '750W', 'AMD Ryzen 7 5800X', '16GB', '1TB', 'GTX 4070ti', 'Phanteks 400p', 'logitech', 'LG 24ML60SP Monitor 24', 'Razer', 2, 'Intel Wifi'),
+  new Desktop('ASUS PRIME B660-PLUS', '1200W', 'AMD Ryzen 7 5800X3D', '32GB', '2TB', 'GTX 4090', 'Phanteks 400p', 'logitech', 'LG 24ML60SP Monitor 32', 'Razer', 2, 'Intel Wifi'),
+  new Desktop('Gigabyte B450 AORUS M', '500W', 'AMD Ryzen 5 3600', '8GB', '500GB', 'GTX 1660', 'Corsair 4000D', 'Razer', 'Dell P2419H 24-inch Monitor', 'Logitech', 3, 'Intel Wifi'),
+  new Laptop('Intel', 'i7 1365g', '16gb', '512gb', 'Iris XE', '15.6', '40wh', 'Lenovo'),
+  new Laptop('Intel', 'i5 1135g', '8gb', '256gb', 'Iris XE', '14', '30wh', 'Dell'),
+  new Laptop('Intel', 'i7 1360h', '16gDDR5', '1Tb', '4050', '17', '80wh', 'Dell'),
+];
--- index.php
+++ index.php
@@ -3,12 +3,13 @@ include_once __DIR__."/models/Computer.php";
 include_once __DIR__."/models/Desktop.php";
 include_once __DIR__."/models/Laptop.php";
 
-$asus = new Desktop('ASUS PRIME B550-PLUS','750W','AMD Ryzen 7 5800X','16GB','1TB','GTX 4070ti','Phanteks 400p','logitech','LG 24ML60SP Monitor 24','Razer',2,'Intel Wifi');
-$lenovo = new Laptop('Intel','i7 1365g','16gb','512gb','Iris XE','15.6','40wh', 'lenovo');
+include "db.php";
 
-var_dump($asus);
 
-var_dump($lenovo);
+var_dump($computers);
+
+
+?>
 
 
 

311c6829c7c9edba627c6630a1fb7cac5625de75
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-17 15:57:41 +0200

    Add bootstrap + cards

--- index.php
+++ index.php
@@ -1,15 +1,61 @@
 <?php
-include_once __DIR__."/models/Computer.php";
-include_once __DIR__."/models/Desktop.php";
-include_once __DIR__."/models/Laptop.php";
+include_once __DIR__ . "/models/Computer.php";
+include_once __DIR__ . "/models/Desktop.php";
+include_once __DIR__ . "/models/Laptop.php";
 
 include "db.php";
 
 
-var_dump($computers);
-
+//var_dump($computers);
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=+, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+  <title>Document</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <h1 class="text-center m-5">Computers</h1>
+    <div class="row">
+      <?php foreach ($computers as $computer) ?>
+      <div class="col-3">
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
